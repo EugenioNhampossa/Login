f19af64cbbe4a45662b399a5befc81d8ab5d1ed4
Eugénio Nhampossa <eugenioanhampossa@gmail.com>
2022-05-16 01:22:59 +0200

    Register and email verification complete

--- app/model/User.php
+++ app/model/User.php
@@ -1,18 +1,26 @@
 <?php
 
-use PHPMailer\PHPMailer\OAuth;
-use  PHPMailer\PHPMailer\PHPMailer;
+use app\model\Mail\Mail;
 
+require_once __DIR__ . '/Mail.php';
 class User
 {
+
+    /**
+     * Creates the user end sends the email verification
+     *
+     * @param  mixed $userData
+     * @return void
+     */
     public static function create($userData)
     {
         //User data:
         $username = trim($userData['username']);
         $email = trim($userData['email']);
         $password = password_hash($userData['password'], PASSWORD_BCRYPT);
-        $vkey = md5(time() . $username);
+        $vkey = md5(time() . "$username");
 
+        //Connecting with databese
         $con = Connection::getConn();
         $sql = "INSERT INTO user (username,email,password,vkey) VALUES (?,?,?,?)";
         $sql = $con->prepare($sql);
@@ -21,42 +29,29 @@ class User
         $sql->bindValue(3, $password, PDO::PARAM_STR);
         $sql->bindValue(4, $vkey, PDO::PARAM_STR);
         $response = $sql->execute();
+        //Verifying the database insertion
+        if ($response > 0) {
+            $html = "<a href='http://localhost/Login/?page=login&method=confirm&vkey=$vkey'>Verify your email</a>";
 
-        if ($response != 0) {
-            $html = "<a href='http://localhost/Login/?page=login&method=confirm'>Verify your email</a>";
-            $mailSended = sendEmail($email, $username, "Email Verification", $html);
+            $mail = new Mail();
+            $mailSended = $mail->sendEmail($email, "Email verification", $html);
         }
 
-        return $mailSended && $response;
+        return $mailSended && $response > 0;
     }
-}
 
-
-
-function sendEmail($to, $name, $subject, $html)
-{
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-
-    $mail->From = "eugenioanhampossa@gmail.com";
-    $mail->FromName = "Login System";
-
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = "465";
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    //$mail->oauthUserEmail = "eugenioanhampossa@gmail.com";
-    //$mail->oauthClientId = "";
-    //$mail->oauthClientSecret = "";
-    $mail->AuthType = "XOUTH2";
-
-    $mail->Username = "eugenioanhampossa@gmail.com";
-    $mail->Password = "nh@mae123";
-
-    $mail->addAddress($to, $name);
-    $mail->Subject = $subject;
-    $mail->AltBody = "To see this message use a program that supports HTML!";
-    $mail->msgHTML($html);
-
-    return $mail->send();
+    public static function confirm($vkey)
+    {
+        try {
+            $con = Connection::getConn();
+            $sql = "UPDATE user SET verified = :n WHERE vkey = :vkey";
+            $sql = $con->prepare($sql);
+            $sql->bindValue(":n", 1, PDO::PARAM_INT);
+            $sql->bindValue(":vkey", $vkey, PDO::PARAM_STR);
+            $response = $sql->execute();
+            return $response > 0;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 }
